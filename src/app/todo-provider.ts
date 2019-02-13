4522b757177411908f776477ec0837ee26242cbe
Pierre Bretéché <pbreteche@dawan.fr>
2019-02-13 16:46:20 +0100

    ajout des tarifs

--- src/app/todo-provider.ts
+++ src/app/todo-provider.ts
@@ -7,18 +7,21 @@ export class TodoProvider {
         title: 'faire du js',
         author: 'Alice',
         endDate: new Date('2019-02-15'),
+        price: 100,
         description: 'coder, coder et toujours coder'
       },
       {
         title: 'acheter du pain',
         author: 'Bod',
         endDate: new Date('2019-12-31'),
+        price: 80,
         description: 'une petite tradi'
-    },
+      },
       {
         title: 'manger du fromage',
         author: 'Alice',
         endDate: new Date('2019-06-21'),
+        price: 120,
         description: 'le gras, c\'est la vie'
       },
     ];
--- src/app/todo.ts
+++ src/app/todo.ts
@@ -2,5 +2,6 @@ export class Todo {
   title: string;
   author: string;
   endDate: Date;
+  price: number;
   description: string;
 }
