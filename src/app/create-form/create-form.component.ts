590e99cf071c31c576a2ee02816dba77beed8fe0
Pierre Bretéché <pbreteche@dawan.fr>
2019-02-14 12:27:10 +0100

    gérer la soumission du formulaire

--- src/app/create-form/create-form.component.ts
+++ src/app/create-form/create-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Todo} from '../todo';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-create-form',
@@ -18,6 +19,12 @@ export class CreateFormComponent implements OnInit {
 
   get today(): string {
     return this.todayDate.toISOString().substr(0, 10);
+  }
+
+  create(form: FormGroup) {
+    // faire quelque chose avec this.todo
+    this.todo = new Todo();
+    form.reset();
   }
 
 }
